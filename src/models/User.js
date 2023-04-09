3a89022a4a46c8490d5071bf322db7ce0240cd8a
Reginaldo Teixeira <reginaldoteixeiraouregis@gmail.com>
2023-04-08 21:52:03 -0300

    Requisite 02 complete (User model in src/models/User.js with correct properties was created)

---
+++ src/models/User.js
@@ -0,0 +1,28 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      allowNull: false, 
+      autoIncrement: true,  
+      type: DataTypes.INTEGER,
+      primaryKey: true
+    },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  },  
+  {
+      timestamps: false,
+      tableName: 'users',
+      underscored: true,
+  });
+
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost, {
+      foreignKey: 'userId',
+      as: 'posts'
+    });
+  };
+
+  return User;
+};
